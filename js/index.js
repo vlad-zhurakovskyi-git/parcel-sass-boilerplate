f258f81b58eaf2283c07682d1d270eafda8ea78b
VZ <vlad.zhurakovskyi@gmail.com>
2020-03-16 10:08:05 +0200

    fix: refactoring

--- js/index.js
+++ js/index.js
@@ -1,10 +1,35 @@
-    // css
+ // js
+import $ from 'jquery';
+import 'slick-carousel/slick/slick';
+
+ $('.references-slider').slick({
+     slidesToShow: 3,
+     slidesToScroll: 3,
+     dots: true,
+     arrows: false,
+ });
+
+
+ $('.price-slider').slick({
+     slidesToShow: 5,
+     slidesToScroll: 1,
+     dots: true,
+     arrows: false,
+ });
+
+ $(window).scroll(function (){
+     $(' .hiw-content__snake').each(function (){
+         var imagePos = $(this).offset().top;
+         var topOfWindow = $(window).scrollTop();
+         if (imagePos < topOfWindow+710) {
+             $(this).addClass('hiw-content__snake_animate');
+         }
+     });
+ });
+
+
+ // css
 import 'normalize.css';
 import './../css/main.scss';
-
-    // js
-import $ from 'jquery';
-
-$(function() {
-    console.log( "ready!" );
-});
+import 'slick-carousel/slick/slick.scss';
+import 'slick-carousel/slick/slick-theme.scss';
